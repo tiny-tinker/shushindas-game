923e9a4fe70a09075e0a92da0ec48711b15aa192
Travis DePuy <travis.depuy@sada.com>
2024-05-06 12:38:38 -0600

    Frontend dev is weird. But it works now!

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,18 @@ variable "region" {
   description = "The region for deploying to."
 }
 
+variable "emb_model" {
+  type = string
+  default = "textembedding-gecko@002"
+  description = "The text embedding model to use for working with embeddings"
+}
+
+variable "bq_region" {
+  type = string
+  default = "US"
+  description = "The region for the BQ assets."
+}
+
 variable "destroy_all" {
   type      = bool
   default   = true
@@ -40,6 +52,40 @@ resource "google_bigquery_dataset" "shushindas_stuff" {
   delete_contents_on_destroy  = var.destroy_all
 
 }
+
+resource "google_bigquery_connection" "connection" {
+    connection_id = "vertex_ai"
+    location = var.bq_region
+    cloud_resource {}
+}
+
+
+resource "google_project_iam_binding" "bq_connection_iam_binding" {
+  project = var.project
+  role    = "roles/aiplatform.user"
+
+  members = [
+    "serviceAccount:${google_bigquery_connection.connection.cloud_resource[0].service_account_id}"
+  ]
+}
+
+# // Create a BigQuery Job to run a SQL query which includes the Model 
+##### unfortunately it looks like this won't work. I got this error:
+##### "Cannot set create disposition in jobs with ML DDL statements"
+#####
+# resource "google_bigquery_job" "query" {
+#   job_id     = "create_model_${uuid()}"
+#   location   = var.bq_region
+
+#   query {
+#     query = <<EOF
+#    CREATE OR REPLACE MODEL shushindas_stuff.embedding_model
+#    REMOTE WITH CONNECTION `${var.bq_region}.${google_bigquery_connection.connection.connection_id}`
+#    OPTIONS (ENDPOINT = '${var.emb_model}')
+# EOF
+#   }
+# }
+
 
 resource "google_bigquery_table" "docs" {
   dataset_id = google_bigquery_dataset.shushindas_stuff.dataset_id
